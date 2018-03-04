e1fde14f3239549fffbac5a75ed8e5d6c1aad53c
smhadmin <sean.henry@supersites.onmicrosoft.com>
2018-03-04 15:44:55 -0600

    UI for mem generator - commit before azure push

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace shenry_mod4cp_030418.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
